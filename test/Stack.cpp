49952a4aa8d89d472b2a40fb1300dc026b32c9d8
greggay <greggay@debian.globalsuite.net>
2012-09-17 21:46:37 -0400

    Created my unit tests.

--- test/Stack.cpp
+++ test/Stack.cpp
@@ -1,12 +1,39 @@
 #include "gtest/gtest.h"
 #include "../src/Stack.h"
 
+TEST(StackTest, Initialization) {
+  Stack* s = new Stack();
+  EXPECT_EQ(0, s->size());
+}
+
 TEST(StackTest, Push) {
   Stack* s = new Stack();
   s->push(6);
-
+  
   EXPECT_EQ(6, s->peek());
   delete s;
 }
 
+TEST(StackTest, PushPastCapacity) {
+  Stack* s = new Stack();
+  //s->push(6);
+  for (int i = 0; i < 10; ++i) {
+  	s->push(i);
+  }
+  EXPECT_EQ(9, s->peek());
+  //delete s;
+}
 
+TEST(StackTest, Resize) {
+  Stack* s = new Stack(6);
+  s->push(1);
+  s->push(2);
+  s->push(3);
+  s->push(4);
+  s->push(5);
+  s->push(6);
+  
+  //EXPECT_EQ(6, s->peek());
+  EXPECT_EQ(6, s->size());
+  delete s;
+}
